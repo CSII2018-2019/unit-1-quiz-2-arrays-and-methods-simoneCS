88877b04f0e29a972336a524946dd55ded74cbf2
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-11-07 08:57:51 -0500

    final code

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 
 public class ArrayTest {
 
-	public ArrayTest() {
+	//public ArrayTest() {
 		// TODO Auto-generated constructor stub
-	}
+	//}
 
 	public static void main(String[] args) {
 	double [] array = {1,2,3,4,5,6,7,8,9,10};
